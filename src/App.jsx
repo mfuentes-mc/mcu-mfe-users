7414646f47383ccd2c785720c58db44f880981ff
Your Name <you@example.com>
2022-04-21 23:19:09 -0600

    refactor(frontend): update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import UsersRouters from './routers/UsersRouters';
 
 const App = () => (
   <div>
-    <UsersRouters/>
+    <BrowserRouter>
+      <UsersRouters/>
+    </BrowserRouter>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
