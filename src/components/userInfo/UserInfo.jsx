ded20eafb09fd76957cdd496fbb49a5345fde924
Your Name <you@example.com>
2022-04-26 16:50:22 -0600

    refactor(frontend): update title for user info

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -4,7 +4,7 @@ const UserInfo = () => {
   return (
     <div className="col-md-6">
       <div className="h-100 p-5 text-white bg-primary rounded-3">
-        <h2>User Micro-frontend Mango Chango University</h2>
+        <h2>User Micro-frontend</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora vitae esse nam, suscipit unde repellat animi pariatur velit quo architecto itaque eveniet non ex earum id necessitatibus, dignissimos sit libero.</p>
       </div>
     </div>
