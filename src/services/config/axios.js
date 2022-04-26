318f30124e02efcf731a68929dec50392b6ee7ba
Your Name <you@example.com>
2022-04-26 13:52:05 -0600

    refactor(frontend): update title user info

--- src/services/config/axios.js
+++ src/services/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: 'http://54.86.153.173:8080/',
+    baseURL: 'http://localhost:3000/',
 });
 
 export default clientAxios;
